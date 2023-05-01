b0ddd165ca5017a2de3506054e401b5edc909ae3
Sa’d Bitsalov <bitsalovss@gmail.com>
2023-05-01 11:50:01 +0300

    _ - add vbyens poject

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import VbyensCounter from "./components/VbyensCounter";
+
+function App() {
+  return (
+    <>
+      <div className="app">
+        <h1>vbyens</h1>
+        <VbyensCounter />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/VbyensCounter.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { render } from "react-dom";
+
+function VbyensCounter() {
+  const now = new Date();
+  const endOfDayinMinutes = 18 * 60;
+  const [seconds, setSeconds] = useState(Date.now());
+  const [counter, setCounter] = useState(
+    endOfDayinMinutes - (now.getHours() * 60 + now.getMinutes())
+  );
+
+  useEffect(() => {
+    setCounter(endOfDayinMinutes - (now.getHours() * 60 + now.getMinutes()));
+  }, [seconds]);
+
+  useEffect(() => {
+    const interval = setInterval(() => setSeconds(Date.now()), 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return <h2>{counter}</h2>;
+}
+
+export default VbyensCounter;
